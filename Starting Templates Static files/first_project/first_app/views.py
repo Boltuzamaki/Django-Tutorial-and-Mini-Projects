d97d698f9b63935bf7f07228952f202f33edc246
boltuzamaki <divyanshuboltuzamaki23@gmail.com>
2021-04-20 01:23:18 +0530

    adding static files static images/css

--- Starting Templates Static files/first_project/first_app/views.py
+++ Starting Templates Static files/first_project/first_app/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 def index(request):
     my_dict = {'insert_me': 'Hello I am from views.py'}
-    return render(request, 'first_app/index.html', context = my_dict)
+    return render(request, 'first_app/.index.html', context = my_dict)

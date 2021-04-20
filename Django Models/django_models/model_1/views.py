a0034d99b99203cc7fd878822faa343fb34ae2bd
boltuzamaki <divyanshuboltuzamaki23@gmail.com>
2021-04-20 14:15:22 +0530

    Views Paradigm access data from models in views

--- Django Models/django_models/model_1/views.py
+++ Django Models/django_models/model_1/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from model_1.models import AccessRecord, Webpage, Topic
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_content': 'Hello I am new here'}
-    return render(request, 'model_1/index.html', context = my_dict)
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_record': webpage_list}
+    return render(request, 'model_1/index.html', context = date_dict)
